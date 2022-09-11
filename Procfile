ea9813b152e948319e2317c4297b0acc9c93cb70
FelipePatitucci <felipepatitucci@gmail.com>
2022-09-11 17:28:12 -0300

    ajuste main e procfile

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python main.py --log-file--
+web: python main.py runserver 0.0.0.0:5000 --log-file--
--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from website import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from os import environ
+    app.run(debug=True, port=environ.get("PORT", 5000), host='0.0.0.0')
